2b60e03575f903013062e06beab9e17022f3deda
Renato Groffe <renato.groffe@yahoo.com.br>
2023-04-20 10:23:25 -0300

    .NET 7 + Azure Durable Functions (Chaining) + Isolated Process + Queue Starter + Azure Queue Storage + Simulação Cotações Dólar
    .NET 7 + Azure Durable Functions (Chaining) + Isolated Process + Queue Starter + Azure Queue Storage + Simulação Cotações Dólar

---
+++ FunctionAppProcessarMoedas/ProcessarSimulacaoDolar.cs
@@ -0,0 +1,28 @@
+﻿using FunctionAppProcessarMoedas.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.Extensions.Logging;
+
+namespace FunctionAppProcessarMoedas;
+
+public static class ProcessarSimulacaoDolar
+{
+    [Function(nameof(ProcessarSimulacaoDolar))]
+    public static async Task RunOrchestrator(
+        [OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var logger = context.CreateReplaySafeLogger(nameof(ProcessarSimulacaoDolar));
+        logger.LogInformation(
+            $"{nameof(ProcessarSimulacaoDolar)} - Iniciando a execucao do metodo {nameof(RunOrchestrator)}...");
+
+        var parametrosExecucao = context.GetInput<ParametrosExecucao>();
+
+        var dadosCotacao = await context.CallActivityAsync<DadosCotacao>(
+            nameof(ActivitySimularCotacaoDolar), parametrosExecucao);
+        await context.CallActivityAsync<DadosCotacao>(
+            nameof(ActivityNotificarAzureQueueStorage), dadosCotacao);
+
+        logger.LogInformation(
+            $"{nameof(ProcessarSimulacaoDolar)} - Concluindo a execucao do metodo {nameof(RunOrchestrator)}.");
+    }
+}
